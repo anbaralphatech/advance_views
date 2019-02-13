b66610c9d29734fd1edbcf73bc5b4dfb578eb975
anbaralphatech <anbar@alphatech.id>
2019-02-13 10:17:39 +0700

    baru

--- Mentee/models.py
+++ Mentee/models.py
@@ -4,11 +4,11 @@ from django.db.models import DateField
 from django.db.models import DateTimeField
 from django.utils import timezone
 from django.db import models as models
-from django.db.models import ImageField
+# from django.db.models import ImageField
 
 # Create your models here.
 
 class Mentee(models.Model):
     name = models.CharField(max_length=100)
     quotes = models.TextField()
-    model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    # model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
